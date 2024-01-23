9b5937785fa3913855c162f33f5c41aeb7986e02
Thomas Gehrmann <s6thgehr@gmail.com>
2024-01-23 20:06:21 +0100

    Calculate correct decay rates by considering duration

--- app/src/types/options.ts
+++ app/src/types/options.ts
@@ -13,10 +13,10 @@ export const categoryOptions = [
 ];
 
 export const decayOptions = [
-  { value: "1", label: "Minutes" }, // ln(e)
-  { value: "2.3", label: "Hours" }, // ln(10)
-  { value: "4.6", label: "Days" }, // ln(100)
-  { value: "6.9", label: "Weeks" }, // ln(1000)
-  { value: "9.2", label: "Months" }, // ln(10000)
-  { value: "11.5", label: "Years" }, // ln(100000)
+  { value: "11.111", label: "Minutes" }, // ln(e) / (1 hour in slots) * 100000
+  { value: "1.065", label: "Hours" }, // ln(10) / (1 day in slots) * 100000
+  { value: "0.304", label: "Days" }, // ln(100) / (1 week in slots) * 100000
+  { value: "0.1065", label: "Weeks" }, // ln(1000) / (30 days in slots) * 100000
+  { value: "0.01167", label: "Months" }, // ln(10000) / (1 year in slots) * 100000
+  { value: "0.0029173", label: "Years" }, // ln(100000) / (5 years in slots) * 100000
 ];
